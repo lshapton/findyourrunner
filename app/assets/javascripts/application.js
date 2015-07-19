7b8a374948c049ebcd9d493eef4b1e2f2e88414a
Leanne Shapton <shaptonleanne@gmail.com>
2015-07-19 14:12:29 -0400

    adding rails files

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,51 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+
+
+var findRunner = {};
+
+var mapStyle = [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
+
+findRunner.loadMap = function(lat, lng) {
+
+  console.log(lat);
+  console.log(lng);
+
+  var mapOptions = {
+    center: { lat: lat, lng: lng},
+    zoom: 15,
+    styles: mapStyle
+  };
+
+  var mapCanvas = $('.map')[0];
+
+  findRunner.map = new google.maps.Map(mapCanvas, mapOptions);
+
+}; 
+
+
+$(function() {
+  // Runner's position
+  navigator.geolocation.getCurrentPosition(function(position) {
+    findRunner.loadMap(position.coords.latitude, position.coords.longitude);
+    }, 
+    function(err){
+      alert('there is an error');
+  });
+}); // end doc ready
+
